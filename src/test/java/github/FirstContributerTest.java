4c2385ddf7aa7071963693bec2cc40090aaafec9
Maxim Kruchkov <mkruchkov@ayist.games>
2021-12-23 14:23:10 +0300

    add 10-3 lecture

---
+++ src/test/java/github/FirstContributerTest.java
@@ -0,0 +1,24 @@
+package github;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class FirstContributerTest {
+
+    @Test
+    void firstContrubuterShouldBeAndreiSolntsev() {
+        open("https://github.com/selenide/selenide");
+        // подвести мышку к первому элементу в области Contributors
+        //$(".Layout-sidebar").$(byText("Contributors")).closest("div")
+        //                .$$("ul li").first().hover();
+
+        $$(".Layout-sidebar .BorderGrid-row").find(text("Contributors"))
+                .$$("ul li").first().hover();
+        // check: в появившемся окошке (оверлей) текст Andrei Solntsev
+        $$(".Popover-message").find(visible).shouldHave(text("Andrei Solntsev"));
+    }
+}
